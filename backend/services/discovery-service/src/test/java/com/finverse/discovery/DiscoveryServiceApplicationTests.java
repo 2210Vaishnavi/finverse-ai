de9852c37b3b2d21bdafe12e675c5ee399681127
65965_Vaishnavi <vaishnavikulkarni2210@gmail.com>
2026-06-28 22:25:27 +0530

    feat(discovery): implement Eureka Discovery Server

---
+++ backend/services/discovery-service/src/test/java/com/finverse/discovery/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,18 @@
+package com.finverse.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(
+    properties = {
+        "eureka.client.register-with-eureka=false",
+        "eureka.client.fetch-registry=false"
+    }
+)
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
